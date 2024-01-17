78e8a3435f65a67608bd6e1f0cd559792ca14f84
Noriny Jullyana <noriny.pessoa@gmail.com>
2024-01-17 20:20:44 -0300

    tabela pesquisa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ApiService } from './app.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'bairesdev';
+  termoBusca: string = '';
+  dados: any[] = [];
+
+  dataSource = new MatTableDataSource<any>();
+  colunas: string[] = ['name', 'username', 'email', 'address', 'phone', 'website', 'company'];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.dadosDaAPI().subscribe(
+      (dadosDaApi) => {
+        this.dataSource.data = dadosDaApi;
+      },
+      (error) => {
+        console.error('Erro ao obter dados da API:', error);
+      }
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.filterPredicate = this.criarFiltro();
+  }
+
+  criarFiltro(): (data: any, filter: string) => boolean {
+    const filtro = (data: any, filter: string): boolean => {
+      const termo = filter.trim().toLowerCase();
+      return Object.values(data).some(value => value && value.toString().toLowerCase().includes(termo));
+    };
+    return filtro;
+  }
+
+  filtrarDados(): void {
+    this.dataSource.filter = this.termoBusca;
+  }
+
+  aoClickEndereco(usuarioId: number, dados: any[]) {
+    const dado = this.dataSource.data.find(u => u.id === usuarioId);
+
+    if (dado) {
+      const endereco = dado.address;
+
+      if (endereco) {
+        const geo = endereco.geo;
+        if (geo) {
+          alert(`Detalhes do Endereço:\nRua: ${endereco.street}\nCidade: ${endereco.city}\nCEP: ${endereco.zipcode}\nLatitude: ${geo.lat}\nLongitude: ${geo.lng}`);
+        } else {
+          alert('Não há informações de geolocalização disponíveis.');
+        }
+      }
+    } else {
+      alert('O array de dados está vazio.');
+    }
+  }
+
+  aoClickEmpresa(usuarioId: number, dados: any[]) {
+    const dado = this.dataSource.data.find(u => u.id === usuarioId);
+
+    if (dado) {
+      const empresa = dado.company;
+
+      if (empresa) {
+        alert(`Detalhes da Empresa:\nNome: ${empresa.name}\nLema: ${empresa.catchPhrase}\nSegmento: ${empresa.bs}`);
+      } else {
+        alert('Não há informações da Empresa disponíveis.');
+      }
+    } else {
+      alert('O array de dados está vazio.');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiService } from './app.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+    
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
